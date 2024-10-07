cce38f8078a708682c2627a321fcafeb7df8ddd5
Pim Brouwers <pimnation@gmail.com>
2024-10-07 09:36:10 -0400

    idbcommand async tests

--- test/Leger.Tests/IDbCommandTests.cs
+++ test/Leger.Tests/IDbCommandTests.cs
@@ -88,3 +88,90 @@ public class IDbCommandTests(TestDb testDb)
         Assert.Single(dt.Rows);
     }
 }
+
+[Collection("TestDb")]
+public class IDbCommandTestsAsync(TestDb testDb)
+{
+    [Fact]
+    public async Task CanExecuteAsync()
+    {
+        using var conn = testDb.CreateConnection();
+        using var cmd = conn.CreateCommand();
+
+        await cmd.ExecuteAsync("""
+            INSERT INTO test_values (description) VALUES (@description);
+            """,
+            new("description", TestDb.GenerateRandomString()));
+
+        Assert.True(true);
+    }
+
+    [Fact]
+    public async Task CanScalarAsync()
+    {
+        var expected = TestDb.GenerateRandomString();
+
+        using var conn = testDb.CreateConnection();
+        using var cmd = conn.CreateCommand();
+
+        var result =
+            await conn.ScalarAsync(
+                "SELECT @description AS description",
+                new("description", expected));
+
+        Assert.Equal(expected, result as string);
+    }
+
+    [Fact]
+    public async Task CanQueryAsync()
+    {
+        var expected = TestDb.GenerateRandomString();
+
+        using var conn = testDb.CreateConnection();
+        using var cmd = conn.CreateCommand();
+
+        var result =
+            await cmd.QueryAsync(
+                "SELECT @description AS description",
+                new("description", expected),
+                rd => rd.ReadString("description"));
+
+        Assert.Equal(expected, result.First());
+    }
+
+    [Fact]
+    public async Task CanQuerySingleAsync()
+    {
+        var expected = TestDb.GenerateRandomString();
+
+        using var conn = testDb.CreateConnection();
+        using var cmd = conn.CreateCommand();
+
+        var result =
+            await cmd.QuerySingleAsync(
+                "SELECT @description AS description",
+                new("description", expected),
+                rd => rd.ReadString("description"));
+
+        Assert.Equal(expected, result);
+    }
+
+    [Fact]
+    public async Task ReadShouldWorkAsync()
+    {
+        var expected = TestDb.GenerateRandomString();
+        var sql = "SELECT 1 AS num1, 2 AS num2";
+        using var conn = testDb.CreateConnection();
+        using var cmd = conn.CreateCommand();
+
+        var dt = await cmd.ReadAsync(sql, rd =>
+        {
+            var dt = new System.Data.DataTable();
+            dt.Load(rd);
+            return dt;
+        });
+
+        Assert.Equal(2, dt.Columns.Count);
+        Assert.Single(dt.Rows);
+    }
+}
