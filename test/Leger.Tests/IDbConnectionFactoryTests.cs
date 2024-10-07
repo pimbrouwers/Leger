5738b2c44c87dfc482cdba14672724307bbf233a
Pim Brouwers <pimnation@gmail.com>
2024-10-07 09:40:35 -0400

    idbtransaction async tests

--- test/Leger.Tests/IDbConnectionFactoryTests.cs
+++ test/Leger.Tests/IDbConnectionFactoryTests.cs
@@ -73,3 +73,75 @@ public class IDbConnectionFactoryTests(TestDb testDb)
         Assert.Single(dt.Rows);
     }
 }
+
+[Collection("TestDb")]
+public class IDbConnectionFactoryTestsAsync(TestDb testDb)
+{
+    [Fact]
+    public async Task CanExecuteAsync()
+    {
+        await testDb.ExecuteAsync("""
+            INSERT INTO test_values (description) VALUES (@description);
+            """,
+            new("description", TestDb.GenerateRandomString()));
+
+        Assert.True(true);
+    }
+
+    [Fact]
+    public async Task CanScalarAsync()
+    {
+        var expected = TestDb.GenerateRandomString();
+
+        var result =
+            await testDb.ScalarAsync(
+                "SELECT @description AS description",
+                new("description", expected));
+
+        Assert.Equal(expected, result as string);
+    }
+
+    [Fact]
+    public async Task CanQueryAsync()
+    {
+        var expected = TestDb.GenerateRandomString();
+
+        var result =
+            await testDb.QueryAsync(
+                "SELECT @description AS description",
+                new("description", expected),
+                rd => rd.ReadString("description"));
+
+        Assert.Equal(expected, result.First());
+    }
+
+    [Fact]
+    public async Task CanQuerySingleAsync()
+    {
+        var expected = TestDb.GenerateRandomString();
+
+        var result =
+            await testDb.QuerySingleAsync(
+                "SELECT @description AS description",
+                new("description", expected),
+                rd => rd.ReadString("description"));
+
+        Assert.Equal(expected, result);
+    }
+
+    [Fact]
+    public async Task ReadShouldWorkAsync()
+    {
+        var expected = TestDb.GenerateRandomString();
+        var sql = "SELECT 1 AS num1, 2 AS num2";
+        var dt = await testDb.ReadAsync(sql, rd =>
+        {
+            var dt = new System.Data.DataTable();
+            dt.Load(rd);
+            return dt;
+        });
+
+        Assert.Equal(2, dt.Columns.Count);
+        Assert.Single(dt.Rows);
+    }
+}
