36b0fd9cce11e7db2dad2a6c2243983a511401bc
Pim Brouwers <pimnation@gmail.com>
2024-10-07 09:39:42 -0400

    idbtransaction async tests

--- test/Leger.Tests/IDbTransactionTests.cs
+++ test/Leger.Tests/IDbTransactionTests.cs
@@ -141,3 +141,144 @@ public class IDbTransactionTests(TestDb testDb)
         Assert.Null(exists);
     }
 }
+
+
+[Collection("TestDb")]
+public class IDbTransactionTestsAsync(TestDb testDb)
+{
+    [Fact]
+    public async Task CanExecuteAsync()
+    {
+        using var conn = testDb.CreateConnection();
+        using var tran = conn.CreateTransaction();
+
+        await tran.ExecuteAsync("""
+            INSERT INTO test_values (description) VALUES (@description);
+            """,
+            new("description", TestDb.GenerateRandomString()));
+
+        tran.Commit();
+        Assert.True(true);
+    }
+
+    [Fact]
+    public async Task CanScalarAsync()
+    {
+        var expected = TestDb.GenerateRandomString();
+
+        using var conn = testDb.CreateConnection();
+        using var tran = conn.CreateTransaction();
+
+        var result =
+            await tran.ScalarAsync(
+                "SELECT @description AS description",
+                new("description", expected));
+
+        tran.Commit();
+        Assert.Equal(expected, result as string);
+    }
+
+    [Fact]
+    public async Task CanQueryAsync()
+    {
+        var expected = TestDb.GenerateRandomString();
+
+        using var conn = testDb.CreateConnection();
+        using var tran = conn.CreateTransaction();
+
+        var result =
+            await tran.QueryAsync(
+                "SELECT @description AS description",
+                new("description", expected),
+                rd => rd.ReadString("description"));
+
+        tran.Commit();
+        Assert.Equal(expected, result.First());
+    }
+
+    [Fact]
+    public async Task CanQuerySingleAsync()
+    {
+        var expected = TestDb.GenerateRandomString();
+
+        using var conn = testDb.CreateConnection();
+        using var tran = conn.CreateTransaction();
+
+        var result =
+            await tran.QuerySingleAsync(
+                "SELECT @description AS description",
+                new("description", expected),
+                rd => rd.ReadString("description"));
+
+        tran.Commit();
+        Assert.Equal(expected, result);
+    }
+
+    [Fact]
+    public async Task ReadShouldWorkAsync()
+    {
+        var expected = TestDb.GenerateRandomString();
+        var sql = "SELECT 1 AS num1, 2 AS num2";
+        using var conn = testDb.CreateConnection();
+        using var tran = conn.CreateTransaction();
+
+        var dt = await tran.ReadAsync(sql, rd =>
+        {
+            var dt = new System.Data.DataTable();
+            dt.Load(rd);
+            return dt;
+        });
+
+        tran.Commit();
+        Assert.Equal(2, dt.Columns.Count);
+        Assert.Single(dt.Rows);
+    }
+
+    [Fact]
+    public async Task CanExecuteTranWithCommitAsync()
+    {
+        using var conn = testDb.CreateConnection();
+        using var tran = conn.CreateTransaction();
+
+        var p = new DbParams("description", TestDb.GenerateRandomString());
+        await tran.ExecuteAsync("""
+            INSERT INTO test_values (description) VALUES (@description);
+            """,
+            p);
+
+        tran.Commit();
+
+        var exists =
+            await conn.QuerySingleAsync("""
+            SELECT description FROM test_values WHERE description = @description;
+            """,
+            p,
+            rd => rd.ReadString("description"));
+
+        Assert.NotNull(exists);
+    }
+
+    [Fact]
+    public async Task CanExecuteTranWithRollbackAsync()
+    {
+        using var conn = testDb.CreateConnection();
+        using var tran = conn.CreateTransaction();
+
+        var p = new DbParams("description", TestDb.GenerateRandomString());
+        await tran.ExecuteAsync("""
+            INSERT INTO test_values (description) VALUES (@description);
+            """,
+            p);
+
+        tran.Rollback();
+
+        var exists =
+            await conn.QuerySingleAsync("""
+            SELECT description FROM test_values WHERE description = @description;
+            """,
+            p,
+            rd => rd.ReadString("description"));
+
+        Assert.Null(exists);
+    }
+}
